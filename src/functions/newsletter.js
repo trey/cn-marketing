a0b266a277d5eb1705e72f859245cc92323712ee
Trey Piepmeier <trey@treypiepmeier.com>
2020-09-17 17:17:48 -0400

    Try to get this form working.

--- src/functions/newsletter.js
+++ src/functions/newsletter.js
@@ -1,9 +1,8 @@
 // https://github.com/netlify/functions/blob/master/src/lambda/hello_slack.js
+const querystring = require('querystring');
+const fetch = require('node-fetch');
 
-import querystring from 'querystring';
-import fetch from 'node-fetch';
-
-exports.handler = async (event, context) => {
+exports.handler = async (event, context, callback) => {
     // Only allow POST
     if (event.httpMethod !== 'POST') {
         return { statusCode: 405, body: 'Method Not Allowed' };
@@ -13,12 +12,15 @@ exports.handler = async (event, context) => {
     const email = params.email;
     const email2 = params.email2;
 
-    if(email2 === undefined) {
+    console.log('email', email);
+    console.log('email2', email2);
+    console.log('url', process.env.NEWSLETTER_URL);
+    console.log('key', `group_${process.env.GROUP}`);
+    console.log('value', process.env.GROUP);
+
+    if(email2 === '') {
         // Send the form to the ESP.
         return fetch(process.env.NEWSLETTER_URL, {
-            headers: {
-                'content-type': 'application/json'
-            },
             method: 'POST',
             body: JSON.stringify(
                 {
@@ -31,15 +33,19 @@ exports.handler = async (event, context) => {
                 statusCode: 200,
 
                 // Redirect to success page.
-                body: `Hopefully you see this message.`
+                body: `Email ${email} hopefully added to the list.`
             }))
             .catch(error => ({
                 // Still redirect to success page.
 
-                // statusCode: 422,
-                // body: `Oops! Something went wrong. ${error}`
+                statusCode: 422,
+                body: `Oops! Something went wrong. ${error}`
             }));
     } else {
         // Still redirect to success page without posting the form.
+        callback(null, {
+            statusCode: 200,
+            body: 'Nope rope.'
+        });
     }
 };
