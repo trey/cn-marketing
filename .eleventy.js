f4ae52017a6cdcab134b0e32333d1650cf215395
Trey Piepmeier <trey@treypiepmeier.com>
2021-01-18 07:30:22 -0500

    Add Netlify redirects to policies.
    https://docs.netlify.com/routing/redirects/

--- .eleventy.js
+++ .eleventy.js
@@ -20,6 +20,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('src/img');
     eleventyConfig.addPassthroughCopy('src/robots.txt');
     eleventyConfig.addPassthroughCopy('src/favicon.ico');
+    eleventyConfig.addPassthroughCopy('_redirects');
 
     eleventyConfig.addPlugin(pluginRss);
     eleventyConfig.addPlugin(typographyPlugin);
